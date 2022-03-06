63e2308abdc651f4d4690c0f4fe18623798d1c98
Boni Lindsley <boni@lindsley.xyz>
2022-03-06 19:57:19 +0000

    add: notes on floating point precision

--- app/main.f90
+++ app/main.f90
@@ -1,9 +1,18 @@
 program hello
+  ! Bring names from other modules into scope.
+  ! Use => to map from a different name.
+  use, intrinsic :: iso_fortran_env, only: sp=>real32, real64
+  use, intrinsic :: iso_c_binding, only: c_float, c_double
+
   implicit none ! Require variable declaration before use.
 
   ! Variable declarationx come before "body".
   integer :: counter
   real :: pi
+  real(sp) :: short_pi ! Explicit precision.
+  real(real64) :: long_pi
+  real(c_float) :: short_e
+  real(c_double) :: long_e
   complex :: two_dimensional
   character :: a_letter
   logical :: no
@@ -15,6 +24,11 @@ program hello
   ! Variables are not initialised automatically.
   cOuNtEr = 1
   pi = 3.14159265
+  ! Underscore suffix for literal kind.
+  short_pi = 3.14159265_sp
+  long_pi = 3.14159265_real64
+  short_e = 2.718281828_c_float
+  long_e = 2.718281828_c_double
   two_dimensional = (100, 200)
   a_letter = 'A'
   a_letter = "B"
